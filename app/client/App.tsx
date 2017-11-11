5675abfd6e1d6ea89b345cc0c6123edd259f58cb
Daniel McKittrick <mckittrickdaniel@gmail.com>
2017-11-11 10:36:36 -0500

    add material-ui

--- app/client/App.tsx
+++ app/client/App.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import Button from 'material-ui/RaisedButton'
 
 class App extends React.Component<{}, {}> {
   public render() {
-    return <h1>joint-todos</h1>;
+    return (
+      <div>
+        <h1>joint-todos</h1>
+        <Button>Test button</Button>
+      </div>    
+    );
   }
 }
 
